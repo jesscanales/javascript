8b4b21994a6f04af92f1d92ea69293d49bd801c9
Jessica Canales <jessica@monkee-boy.com>
2015-04-04 15:52:21 -0500

    This is a javascript mad lib script

---
+++ story.js
@@ -0,0 +1,5 @@
+var firstResponse = prompt("Give me a type of animal");
+var secondResponse = prompt("Give me a place");
+var thirdResponse = prompt("Give me an adjective");
+
+document.write("Today I woke up and decided it was time to walk my " + firstResponse + ". I got my pet, and we went to the " + secondResponse + ". We had a " + thirdResponse + " time.");
